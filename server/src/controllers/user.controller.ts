0ef71e7ee5346d60b69c8321921bdedbecd11689
Amit Kushwaha <amitcoding56@gmail.com>
2025-08-22 12:45:12 +0530

    User Controller Implement

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import validator from "validator";
 import jwt from "jsonwebtoken";
 
+//Register User
 export const registerUser = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
@@ -67,11 +68,166 @@ export const registerUser = async (req: Request, res: Response) => {
   }
 };
 
+//Login User
 export const loginUser = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      message: "Login Successfully",
+      success: true,
+      user: { id: user._id, name: user.name, email: user.email },
+      token,
+    });
+  } catch (error) {
+    console.log("Error in logging user", error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
+  }
 };
 
+//Get Current User
+export const getCurrentUser = async (req:Request, res:Response) => {
+  try {
+  const user =  await User.findById(req.user.id).select("name email")
+
+  if(!user) {
+    return res.status(404).json({
+      message: "User not found",
+      success: false
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    user
+  })
+  } catch (error) {
+    console.log("Error in getting current user", error);
+    return res.status(500).json({
+      message:  "Internal server error",
+      success: false
+    })
+  }
+}
+
+//Update User Profile
+export const updateProfile = async (req: Request, res:Response) => {
+  try {
+    const {name, email} = req.body
+
+    if(!name || !email || !validator.isEmail(email)) {
+      return res.status(400).json({
+        message: "Valid name and email required",
+        success: false
+      })
+    }
+
+    const exists = await User.findOne(email, _id: {$ne: req.user.id})
+
+    if(exists) {
+      return res.status(400).json({
+        message: "email already in use by another account",
+        success: false
+      })
+    }
+    const update = await User.findByIdAndUpdate(
+      req.user.id,
+      {name, email},
+      {new: true, runValidators: true, select: "new email"}
+    )
+
+    res.status(500).json({
+      success: true,
+      user
+    })
+  } catch (error) {
+    console.log("Error in updating user profile", error)
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false
+    })
+  }
+}
+
+//Change Password
+export const changePassword = async (req: Request, res: Response) => {
+  try {
+    const {currentPaasword, newPassword}  = req.body
+   
+  if(!currentPaasword || !newPassword || newPassword.length < 8) {
+    return res.status(400).json({
+      message: "Password invalid or too short",
+      success: false
+    })
+  }
+
+  const user = await User.findById(req.user.id).select("password")
+
+  if(!user) {
+    return res.status(404).json({
+      message: "User not found",
+      success: false
+    })
+  }
+
+
+  const isMatch = await bcrypt.compare(currentPaasword, user.password)
+  if(!isMatch) {
+    return res.status(401).json({
+      message: "Current password incorrect",
+      success: false
+    })
+  }
+
+  user.password = await bcrypt.hash(newPassword, 10)
+  await user.save()
+
+  res.status(200).json({
+    message: "Password changed",
+    success: true
+  })
+  } catch (error) {
+    console.log("Error in changing password", error)
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false
+    })
+  }
+}
+
+
+//Logout User
 export const logoutUser = async (req: Request, res: Response) => {
   try {
   } catch (error) {}
